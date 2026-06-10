0b25a4002ee442c40c093496c9e324c31fd2c7e4
daniel-guerrero-dev <danielguerrerodev6@gmail.com>
2026-06-10 10:11:26 -0500

    Variable study using javascript.info

---
+++ js-learning/step1-fundamentals/variables/notes.md
@@ -0,0 +1,10 @@
+Variable declaration is done with the keyword `let`
+
+you can declare empty variables and declare their values later or declare them in one line:
+
+```js
+    let name;
+    name = Daniel;
+
+    let number = 5;
+```
---
+++ js-learning/step1-fundamentals/variables/variable.js
@@ -0,0 +1,18 @@
+// Task 1
+
+let admin;
+let name = "Jhon";
+
+admin = name;
+
+alert(admin);
+
+
+// Task 2
+
+let planetName = "Earth";
+let currentVisitor = " name here ";
+
+// Task 3
+
+// Hard coded constants get the uppercase treatment and they only. Not infered constants.
